94a4289dadd5c42f21aaf5769b36041d0da9af48
Israel Abebe <se.israel.abebe@gmail.com>
2020-01-28 10:27:22 +0000

    added downloader

---
+++ data/data.sh
@@ -0,0 +1,17 @@
+wget -nc http://www.vision.caltech.edu/visipedia-data/CUB-200/images.tgz
+wget -nc http://www.vision.caltech.edu/visipedia-data/CUB-200/lists.tgz
+wget -nc http://www.vision.caltech.edu/visipedia-data/CUB-200/annotations.tgz
+wget -nc http://www.vision.caltech.edu/visipedia-data/CUB-200/attributes.tgz
+wget -nc http://www.vision.caltech.edu/visipedia-data/CUB-200/attributes-yaml.tgz
+wget -nc http://www.vision.caltech.edu/visipedia-data/CUB-200/README.txt
+
+tar -xvzf images.tgz
+tar -xvzf lists.tgz
+tar -xvzf annotations.tgz
+tar -xvzf attributes.tgz
+tar -xvzf attributes-yaml.tgz
+
+
+
+
+
